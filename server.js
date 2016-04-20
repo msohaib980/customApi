1007c3e08eae5d21a7ad64667c0669c054b2923f
muhamd sohaib <msohaib980@gmail.com>
2016-04-19 18:04:13 -0700

    Mathsection code is working in server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const PORT = 3000;
+
+var http = require('http');
+
+var server = http.createServer((req, res) => {
+  var params = req.url.split('/');
+  params.shift(); // throwing out the empty string
+  var task = params.shift();
+
+  switch(task) {
+    case 'square':
+      var num = params[0];
+      var square = Math.pow(num, 2);
+      res.write(`${square}`);
+      break;
+    case 'cube':
+      var num = params[0];
+      var cube = Math.pow(num, 3);
+      res.write(`${cube}`);
+      break;
+    case 'pow':
+      var base = params[0];
+      var exponent = params[1];
+      var pow = Math.pow(base, exponent);
+      res.write(`${pow}`);
+      break;
+    case 'div':
+      var num = Number(params[0]);
+      var diveder =Number(params[1]);
+      var div = num/diveder;
+      res.write(`${div}`);
+      break;
+    case 'sum':
+      var sum = params.reduce((acc, num) => {
+        return acc + parseInt(num);
+      }, 0);
+      res.write(`${sum}`)
+      break;
+    case 'mul':
+      var mul = params.reduce((acc, num) => {
+        return acc * parseInt(num);
+      }, 1);
+      res.write(`${mul}`)
+      break;
+    default:
+      res.write('Defadult.');
+  }
+
+  res.end('\n');
+});
+
+server.listen(PORT, function(err) {
+  if(err) return console.log('error!:', err);
+  console.log(`Node server listening on port ${PORT}`);
+});
